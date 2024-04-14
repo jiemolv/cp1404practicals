77e9c71a7b7a0b86edd9878b74760424fede6669
译文同学 <yiwen.zhang@my.jcu.edu.au>
2024-04-14 21:40:44 +0800

    prac 9

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,17 @@
+from prac_09.unreliable_car import UnreliableCar
+
+# Create a new unreliable car object
+unreliable_car = UnreliableCar("Unreliable", 100, 50)
+
+# Print the car's details
+print(f"Unreliable car's details: {unreliable_car}")
+
+# Drive the unreliable car for a distance of 40 km
+distance_driven = unreliable_car.drive(40)
+
+# Print the Distance driven
+print(f"Distance driven: {distance_driven} km")
+
+
+
+
